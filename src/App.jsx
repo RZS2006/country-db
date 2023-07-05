b9a15ae1e6666811daf8a81c9f92a9c07aeed651
Raiyan Shamim <razash139@gmail.com>
2023-07-05 20:23:48 +0300

    remove old code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import {
 	BrowserRouter as Router,
 	Routes,
@@ -13,120 +13,11 @@ import Details from './pages/Details/Details';
 import Alert from './components/Alert/Alert';
 import Loading from './components/Loading/Loading';
 
-import { getCountries } from './api';
-
-import CountriesProvider, {
-	CountriesContext,
-	useCountries,
-} from './contexts/CountriesContext';
+import { useCountries } from './contexts/CountriesContext';
 
 // Component
 const App = () => {
-	const {
-		methods: { toggleFavoriteStatus },
-		loading,
-		error,
-	} = useCountries();
-
-	// State
-	// const [countries, setCountries] = useState();
-	// const [favoritedCountries, setFavoritedCountries] = useState();
-	// const [isLoading, setIsLoading] = useState(true);
-	// const [hasError, setHasError] = useState(false);
-
-	// // Side Effects
-	// useEffect(() => {
-	// 	const fetchCountries = async () => {
-	// 		try {
-	// 			const countries = await getCountries();
-	// 			const countriesWithId = countries.map((country) => ({
-	// 				...country,
-	// 				id: country.cca3,
-	// 			}));
-
-	// 			const localStorageData = JSON.parse(
-	// 				localStorage.getItem('favoritedCountries')
-	// 			);
-
-	// 			if (localStorageData !== null) {
-	// 				const countriesWithFavorited = countriesWithId.map(
-	// 					(country) => ({
-	// 						...country,
-	// 						favorited: localStorageData[country.id],
-	// 					})
-	// 				);
-
-	// 				countriesWithFavorited.sort((a, b) =>
-	// 					a.name.common
-	// 						.toString()
-	// 						.localeCompare(b.name.common.toString())
-	// 				);
-
-	// 				setCountries(countriesWithFavorited);
-	// 				setFavoritedCountries({ ...localStorageData });
-	// 			} else {
-	// 				const favoritedCountriesObject = {};
-	// 				countriesWithId.forEach((country) => {
-	// 					return (favoritedCountriesObject[country.id] = false);
-	// 				});
-
-	// 				const countriesWithFavorited = countriesWithId.map(
-	// 					(country) => ({
-	// 						...country,
-	// 						favorited: favoritedCountriesObject[country.id],
-	// 					})
-	// 				);
-
-	// 				countriesWithFavorited.sort((a, b) =>
-	// 					a.name.common
-	// 						.toString()
-	// 						.localeCompare(b.name.common.toString())
-	// 				);
-
-	// 				setCountries(countriesWithFavorited);
-	// 				setFavoritedCountries(favoritedCountriesObject);
-	// 			}
-
-	// 			setIsLoading(false);
-	// 		} catch (e) {
-	// 			setCountries([]);
-	// 			setFavoritedCountries({});
-	// 			setIsLoading(false);
-	// 			setHasError(true);
-	// 			console.error(e);
-	// 		}
-	// 	};
-
-	// 	fetchCountries();
-	// }, []);
-
-	// const isInitialMount = useRef(true);
-	// useEffect(() => {
-	// 	if (isInitialMount.current) {
-	// 		isInitialMount.current = false;
-	// 	} else {
-	// 		localStorage.setItem(
-	// 			'favoritedCountries',
-	// 			JSON.stringify(favoritedCountries)
-	// 		);
-	// 	}
-	// }, [favoritedCountries]);
-
-	// Functions
-	// const toggleFavoriteStatus = (id) => {
-	// 	const updatedFavoritedCountries = { ...favoritedCountries };
-	// 	updatedFavoritedCountries[id] = !updatedFavoritedCountries[id];
-	// 	setFavoritedCountries(updatedFavoritedCountries);
-
-	// 	const updatedCountries = countries.map((country) => {
-	// 		if (country.id === id) {
-	// 			country.favorited = !country.favorited;
-	// 		}
-	// 		return country;
-	// 	});
-
-	// 	setCountries(updatedCountries);
-	// };
+	const { loading, error } = useCountries();
 
 	// Render
 	if (loading) {
